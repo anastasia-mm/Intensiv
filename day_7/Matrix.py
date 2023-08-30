7f563b223feff0f47d02e14fcef53fa0edfef5b3
Latyshev Pavel zhestki tip <trevor.015@mail.ru>
2023-08-30 15:10:56 +0300

    matrix

---
+++ day_7/Matrix.py
@@ -0,0 +1,12 @@
+import random
+n = 3
+m = 4
+matrix = [[random.randint(10, 100) for j in range(m)]for i in range(n)]
+print(matrix)
+sl = ''
+for i in range(n):
+    for j in range(m):
+        sl += str(matrix[i][j])
+        sl += '  '
+    print(sl)
+    sl = ''
