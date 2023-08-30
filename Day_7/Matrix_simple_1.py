f41e7996ecd430df86757f799c50541fc17471a8
anastasia <martynova.anastasiia@gmail.com>
2023-08-30 15:36:16 +0300

    matrix commit 2

--- Day_7/Matrix_simple_1.py
+++ Day_7/Matrix_simple_1.py
@@ -4,8 +4,8 @@ m = 4
 matrix = [[random.randint(1, 10) for i in range(m)] for j in range(n)]
 print(matrix)
 for line in matrix:
-    for column in line:
-        print("%4d" % column, end=' ')
+    for element in line:
+        print("%4d" % element, end=' ')
     print()
 print()
 print('\n'.join([' '.join('{:{width}}'.format(element, width=4) for element in line) for line in matrix]))
